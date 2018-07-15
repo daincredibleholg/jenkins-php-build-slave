7ab0d480f6367b42b9620f13d0b28114a55c79d7
Holger Steinhauer <holger+github@steinhauer.se>
2018-07-15 19:05:50 +0100

    Switching to OpenJDK 8 means this needs to change as well
    Signed-off-by: Holger Steinhauer <holger+github@steinhauer.se>

--- run-jenkins.sh
+++ run-jenkins.sh
@@ -3,4 +3,4 @@
 ## Get latest agent
 /usr/bin/wget "${JENKINS_MASTER}/jnlpJars/agent.jar" -O /home/jenkins/agent.jar
 
-/opt/openjdk/jdk-10/bin/java -jar /home/jenkins/agent.jar -jnlpUrl "${JENKINS_MASTER}/computer/${COMPUTER_NAME}/slave-agent.jnlp" -secret "$JENKINS_SECRET" -workDir "${WORK_DIR}"
+java -jar /home/jenkins/agent.jar -jnlpUrl "${JENKINS_MASTER}/computer/${COMPUTER_NAME}/slave-agent.jnlp" -secret "$JENKINS_SECRET" -workDir "${WORK_DIR}"
